c3ecdc395d580c8fdcc80957d352380d36240163
Maryia Ondra <ya.ondra@yandex.by>
2020-12-06 12:45:14 +0300

    finished main tast

---
+++ SolarSystem/Assets/Scripts/Planet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed;
+    [SerializeField] private float _planetSpeed;
+    [SerializeField] private float _scaleFactor;
+    [SerializeField] private GameObject _rotationCenter;
+
+    private void Start()
+    {
+        transform.localScale = transform.localScale * _scaleFactor;        
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, _rotateSpeed * Time.deltaTime);
+
+        transform.RotateAround(_rotationCenter.transform.position, Vector3.up, _planetSpeed * Time.deltaTime);
+    }
+}
